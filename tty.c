b2ea59bdb9339141e2aebdff7e2fcb99cceb1e23
Suraj N. Kurapati <sunaku@riseup.net>
2015-10-22 18:57:30 -0700

    I think where you have:
              gc->flags &= ~GRID_FLAG_BG256;
              gc->flags &= ~GRID_FLAG_BGRGB;
    
    It would be better just as gc->flags &= ~(GRID_FLAG_BG256|GRID_FLAG_BGRGB).

--- tty.c
+++ tty.c
@@ -1492,8 +1492,7 @@ tty_colours(struct tty *tty, const struct grid_cell *gc)
 				    tc->flags & (GRID_FLAG_FG256|GRID_FLAG_FGRGB))
 					tty_putcode1(tty, TTYC_SETAF, 7);
 				tc->fg = 8;
-				tc->flags &= ~GRID_FLAG_FG256;
-				tc->flags &= ~GRID_FLAG_FGRGB;
+				tc->flags &= ~(GRID_FLAG_FG256|GRID_FLAG_FGRGB);
 			}
 			if (bg_default &&
 			    (tc->bg != 8 || tc->flags & (GRID_FLAG_BG256|GRID_FLAG_BGRGB))) {
@@ -1503,8 +1502,7 @@ tty_colours(struct tty *tty, const struct grid_cell *gc)
 				    tc->flags & (GRID_FLAG_BG256|GRID_FLAG_BGRGB))
 					tty_putcode1(tty, TTYC_SETAB, 0);
 				tc->bg = 8;
-				tc->flags &= ~GRID_FLAG_BG256;
-				tc->flags &= ~GRID_FLAG_BGRGB;
+				tc->flags &= ~(GRID_FLAG_BG256|GRID_FLAG_BGRGB);
 			}
 		}
 	}
@@ -1627,8 +1625,7 @@ tty_colours_fg(struct tty *tty, const struct grid_cell *gc)
 	u_char			 fg = gc->fg;
 	char			 s[32];
 
-	tc->flags &= ~GRID_FLAG_FG256;
-	tc->flags &= ~GRID_FLAG_FGRGB;
+	tc->flags &= ~(GRID_FLAG_FG256|GRID_FLAG_FGRGB);
 
 	/* Is this a 24-bit colour? */
 	if (gc->flags & GRID_FLAG_FGRGB) {
@@ -1662,8 +1659,7 @@ tty_colours_fg(struct tty *tty, const struct grid_cell *gc)
 save_fg:
 	/* Save the new values in the terminal current cell. */
 	tc->fg = fg;
-	tc->flags &= ~GRID_FLAG_FGRGB;
-	tc->flags &= ~GRID_FLAG_FG256;
+	tc->flags &= ~(GRID_FLAG_FGRGB|GRID_FLAG_FG256);
 	tc->flags |= gc->flags & GRID_FLAG_FG256;
 }
 
@@ -1674,8 +1670,7 @@ tty_colours_bg(struct tty *tty, const struct grid_cell *gc)
 	u_char			 bg = gc->bg;
 	char			 s[32];
 
-	tc->flags &= ~GRID_FLAG_BG256;
-	tc->flags &= ~GRID_FLAG_BGRGB;
+	tc->flags &= ~(GRID_FLAG_BG256|GRID_FLAG_BGRGB);
 
 	/* Is this a 24-bit colour? */
 	if (gc->flags & GRID_FLAG_BGRGB) {
@@ -1709,8 +1704,7 @@ tty_colours_bg(struct tty *tty, const struct grid_cell *gc)
 save_bg:
 	/* Save the new values in the terminal current cell. */
 	tc->bg = bg;
-	tc->flags &= ~GRID_FLAG_BGRGB;
-	tc->flags &= ~GRID_FLAG_BG256;
+	tc->flags &= ~(GRID_FLAG_BGRGB|GRID_FLAG_BG256);
 	tc->flags |= gc->flags & GRID_FLAG_BG256;
 }
 
